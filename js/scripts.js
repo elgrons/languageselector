27c747e01e0c264b4e4d6496a608d89a6e3ee538
Eliot Gronstal <eliot.lauren@gmail.com>
2023-01-13 11:55:52 -0800

    WIP: html and javascript form communication.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,8 @@
-//Business Logic
-//function handleRadio(event) {
-  //event.preventDefault();
-  //const radioSelection = document.querySelector("input[name='animal']:checked").value;
-//}
-
 // User Interface Logic
 
 function hideResults() {
-  document.getElementById("python").setAttribute("class", "hidden");
   document.getElementById("ruby").setAttribute("class", "hidden");
+  document.getElementById("python").setAttribute("class", "hidden");
   document.getElementById("rust").setAttribute("class", "hidden");
   document.getElementById("sorry").setAttribute("class", "hidden");
 }
@@ -16,50 +10,51 @@ function hideResults() {
 function hideResultsAndError() {
   // New line of code to hide the error message.
   document.getElementById("error-message").setAttribute("class", "hidden");
-  document.getElementById("python").setAttribute("class", "hidden");
   document.getElementById("ruby").setAttribute("class", "hidden");
+  document.getElementById("python").setAttribute("class", "hidden");
   document.getElementById("rust").setAttribute("class", "hidden");
   document.getElementById("sorry").setAttribute("class", "hidden");
 }
   
 window.onload = function() {
-  document.querySelector("form").onsubmit = function(event) {
+  document.getElementById("radio-form").onsubmit = function(event) {
     event.preventDefault();
     hideResults();
     hideResultsAndError();
-    const radioSelection1 = document.querySelector("input[name='animal']:checked").value;
-    const radioSelection2 = document.querySelector("input[name='allergy']:checked").value;
-    const radioSelection3 = document.querySelector("input[name='soup']:checked").value;
-    const radioSelection4 = document.querySelector("input[name='shoes']:checked").value;
-    const radioSelection5 = document.querySelector("input[name='age']:checked").value;
-  if (!age || !height) {
-    document.getElementById("error-message").removeAttribute("class");
+    const radioSelection1 = document.getElementById("input[name='animal']:checked").value;
+    const radioSelection2 = document.getElementById("input[name='allergy']:checked").value;
+    const radioSelection3 = document.getElementById("input[name='soup']:checked").value;
+    const radioSelection4 = document.getElementById("input[name='shoes']:checked").value;
+    const radioSelection5 = document.getElementById("input[name='age']:checked").value;
+
+  if (radioSelection1 = radioSelection2) {
+    document.getElementById("radio-form").removeAttribute("class");
     } else {
-    if ("animal" = 'cat' + "soup" = 'eat') {
+    if (radioSelection3 = radioSelection3) {
       document.getElementById("ruby").removeAttribute("class");
+      //document.getElementById("python").removeAttribute("class");
+      //document.getElementById("rust").removeAttribute("class");
+      }
+      else if (radioSelection1 = radioSelection3) {
       document.getElementById("python").removeAttribute("class");
-      document.getElementById("rust").removeAttribute("class");
+      //document.getElementById("rust").removeAttribute("class");
       }
-      else if (age >= 12 || height >= 48) {
-      document.getElementById("python").removeAttribute("class");
-      document.getElementById("rust").removeAttribute("class");
+      //else if (age >= 6) {
+        //document.getElementById("rust").removeAttribute("class");
       }
-      else if (age >= 6) {
-        document.getElementById("rust").removeAttribute("class");
-      }
-      else {
-        document.getElementById("sorry").removeAttribute("class");
+      //else {
+        //document.getElementById("sorry").removeAttribute("class");
       }
     }
-  };
-};
+ // };
+//};
 
 //radio form
-function handleRadio(event) {
-  event.preventDefault();
-  const radioSelection = document.querySelector("input[name='flavor']:checked").value;
-}
+//function handleRadio(event) {
+ // event.preventDefault();
+  //const radioSelection = document.querySelector("input[name='flavor']///:checked").value;
+//}
 
-window.addEventListener("load", function() {
-  document.getElementById("radio-form").addEventListener("submit", handleRadio);
-});
+//window.addEventListener("load", function() {
+ // document.getElementById("radio-form").addEventListener("submit", handleRadio);
+//});
