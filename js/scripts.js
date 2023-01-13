c3b9a1dad89d0e80b6ab8d528de89c543cc1fa84
Eliot Gronstal <eliot.lauren@gmail.com>
2023-01-13 14:19:10 -0800

    refashion html and java again to create functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,14 @@
 // User Interface Logic
 
-function hideResults() {
-  document.getElementById("ruby").setAttribute("class", "hidden");
-  document.getElementById("python").setAttribute("class", "hidden");
-  document.getElementById("rust").setAttribute("class", "hidden");
-  document.getElementById("sorry").setAttribute("class", "hidden");
-}
-
-function hideResultsAndError() {
-  // New line of code to hide the error message.
-  document.getElementById("error-message").setAttribute("class", "hidden");
-  document.getElementById("ruby").setAttribute("class", "hidden");
-  document.getElementById("python").setAttribute("class", "hidden");
-  document.getElementById("rust").setAttribute("class", "hidden");
-  document.getElementById("sorry").setAttribute("class", "hidden");
-}
-  
 window.onload = function() {
   document.getElementById("radio-form").onsubmit = function(event) {
     event.preventDefault();
     hideResults();
     hideResultsAndError();
-    const radioSelection1 = document.getElementById("input[name='animal']:checked").value;
-    const radioSelection2 = document.getElementById("input[name='allergy']:checked").value;
-    const radioSelection3 = document.getElementById("input[name='soup']:checked").value;
-    const radioSelection4 = document.getElementById("input[name='shoes']:checked").value;
-    const radioSelection5 = document.getElementById("input[name='age']:checked").value;
+    const quest1 = document.querySelector("input[name='animal']:checked").value;
+    const quest2 = document.querySelector("input[name='allergy']:checked").value;
+    const quest3 = document.querySelector("input[name='soup']:checked").value;
+    const quest4 = document.querySelector("input[name='shoes']:checked").value;
 
   if (radioSelection1 = radioSelection2) {
     document.getElementById("radio-form").removeAttribute("class");
@@ -49,6 +32,29 @@ window.onload = function() {
  // };
 //};
 
+//function hideResults() {
+  document.getElementById("ruby").setAttribute("class", "hidden");
+  document.getElementById("python").setAttribute("class", "hidden");
+  document.getElementById("rust").setAttribute("class", "hidden");
+  document.getElementById("sorry").setAttribute("class", "hidden");
+///}
+
+//function hideResultsAndError() {
+  // New line of code to hide the error message.
+  document.getElementById("error-message").setAttribute("class", "hidden");
+  document.getElementById("ruby").setAttribute("class", "hidden");
+  document.getElementById("python").setAttribute("class", "hidden");
+  document.getElementById("rust").setAttribute("class", "hidden");
+  document.getElementById("sorry").setAttribute("class", "hidden");
+//}
+
+//window.addEventListener("load", function() {
+  //document.getElementById("radio-form").addEventListener("submit", handleRadio);
+ // event.preventDefault();
+ // hideResults();
+ // hideResultsAndError();
+//});
+
 //radio form
 //function handleRadio(event) {
  // event.preventDefault();
@@ -58,3 +64,24 @@ window.onload = function() {
 //window.addEventListener("load", function() {
  // document.getElementById("radio-form").addEventListener("submit", handleRadio);
 //});
+
+//from calculator example
+// User Interface Logic
+function handleCalculation(event) {
+  event.preventDefault();
+  const number1 = parseInt(document.querySelector("input#input1").value);
+  const number2 = parseInt(document.querySelector("input#input2").value);
+  const operator = document.querySelector("input[name='operator']:checked").value;
+
+  let result;
+  if (operator === "add") {
+    result = add(number1, number2);
+  } else if (operator === "subtract") {
+    result = subtract(number1, number2);
+  } else if (operator === "multiply") {
+    result = multiply(number1, number2);
+  } else if (operator === "divide") {
+    result = divide(number1, number2);
+  }
+
+  document.getElementById("output").innerText = result;
