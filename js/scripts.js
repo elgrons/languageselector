692da58109ad6c8a694a812151b3eae0d739805c
Eliot Gronstal <eliot.lauren@gmail.com>
2023-01-15 08:23:00 -0800

    update branching and now all quiz optioptional results work.

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,32 @@
+//User Interface Logic
+function hideResults() {
+  document.getElementById("ruby").setAttribute("class", "hidden");
+  document.getElementById("python").setAttribute("class", "hidden");
+  document.getElementById("rust").setAttribute("class", "hidden");
+  }
+
 function getResults(event) {
   event.preventDefault();
-  const animal = document.querySelector("input[name='animal']:checked").value;
-  console.log(animal);
-  const allergy = document.querySelector("input[name='allergy']:checked").value;
-  console.log(allergy);
-  const soup = document.querySelector("input[name='soup']:checked").value;
-  console.log(soup);
-  const shoes = document.querySelector("input[name='shoes']:checked").value;
-  console.log(shoes);
+  const animal = parseInt(document.querySelector("input[name='animal']:checked").value);
+  const allergy = parseInt(document.querySelector("input[name='allergy']:checked").value);
+  const soup = parseInt(document.querySelector("input[name='soup']:checked").value);
+  const shoes = parseInt(document.querySelector("input[name='shoes']:checked").value);
+  const resultRange = animal + allergy + soup + shoes;
+console.log(resultRange)
+  if (resultRange <= 4) {
+    document.getElementById("ruby").removeAttribute("class");
+  } 
+    else if (resultRange >4 || resultRange <7) {
+    document.getElementById("python").removeAttribute("class");
+  }
+    else if (resultRange >= 7) {
+      document.getElementById("rust").removeAttribute("class");
+  }
+
+  //document.getElementById('language-form').reset();
 };
 
 window.addEventListener("load", function() {
   const form = document.getElementById("language-form")
   form.addEventListener("submit", getResults);
 });
-
-
-// function hideResults() {
-//   document.getElementById("ruby").setAttribute("class", "hidden");
-//   document.getElementById("python").setAttribute("class", "hidden");
-//   document.getElementById("rust").setAttribute("class", "hidden");
-//   //document.getElementById("sorry").setAttribute("class", "hidden");
-// }
-
-// //document.querySelector("form").onsubmit = function(event) {
-//   hideResults();
-//   event.preventDefault();
-//   const age = parseInt(document.querySelector("input#age").value);
-//   const height = parseInt(document.querySelector("input#height").value);
-
-//  // if () {
-//     document.getElementById("ruby").removeAttribute("class");
-//     document.getElementById("python").removeAttribute("class");
-//     document.getElementById("rust").removeAttribute("class");
-//   }// else if () {
-//     document.getElementById("python").removeAttribute("class");
-//     document.getElementById("rust").removeAttribute("class");
-//   }//else if () {
-//     document.getElementById("rust").removeAttribute("class");
-  
-//   } //else {
-//     document.getElementById("sorry").removeAttribute("class");
-//   }
-// };
