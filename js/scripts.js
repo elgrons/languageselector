957dff4f4a5e2e74f1726b62442e354dbeb95f6d
Eliot Gronstal <eliot.lauren@gmail.com>
2023-02-05 11:21:05 -0800

    update addEventlistener.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,22 @@
 //User Interface Logic
+
 function hideResults() {
   document.getElementById("ruby").setAttribute("class", "hidden");
   document.getElementById("python").setAttribute("class", "hidden");
   document.getElementById("rust").setAttribute("class", "hidden");
   }
 
+  function clearRadioButtons(){
+    const buttons = document.querySelectorAll("input[type=radio]");
+    for(const i=0;i<ele.length;i++){
+        buttons[i].checked = false;
+    }
+  }
+
 function getResults(event) {
-  let form = document.getElementById("language-form")
   event.preventDefault();
+  let form = document.getElementById("language-form");
+
   const animal = parseInt(document.querySelector("input[name='animal']:checked").value);
   const allergy = parseInt(document.querySelector("input[name='allergy']:checked").value);
   const soup = parseInt(document.querySelector("input[name='soup']:checked").value);
@@ -21,22 +30,17 @@ function getResults(event) {
     document.getElementById("python").removeAttribute("class");
   }
     else if (resultRange >= 8) {
-      document.getElementById("rust").removeAttribute("class");
+    document.getElementById("rust").removeAttribute("class");
     };
+  };
 
-  form.addEventListener("submit", getResults), function() {
+  window.addEventListener("load", function() {
+      document.querySelector("form#language-form").addEventListener("submit", getResults);
     let form = document.getElementById("language-form")
-    reset.removeAttribute("class");
-  }
-};
-
-window.addEventListener("load", function() {
-  let form = document.getElementById("language-form")
-  let resetButton = document.getElementById("reset");
-  form.addEventListener("submit", getResults);
-
-  resetButton.addEventListener("click", function() {
-    document.getElementById("language-form").reset();
-    form.addEventListener("reset", hideResults);
-  });
+    let resetButton = document.getElementById("reset");
+  
+    resetButton.addEventListener("click", function() {
+      document.getElementById("language-form").reset();
+      form.addEventListener("reset", hideResults);
+    });
 });
